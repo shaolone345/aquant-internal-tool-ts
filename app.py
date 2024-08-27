fdc8b8c3cb5f8072163c7d5abb05686312f99ef2
shaolone345 <91429396+shaolone345@users.noreply.github.com>
2024-08-27 10:35:11 -0400

    Update and rename streamlit_app.py to app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import decrypt
+import app2
+import app3
+from utils import check_login
+
+def main():
+    if check_login():
+        st.sidebar.title("Navigation")
+        app_choice = st.sidebar.radio("Choose an app:", ("Decrypt", "App 2", "App 3"))
+
+        if app_choice == "Decrypt":
+            decrypt.run()
+        elif app_choice == "App 2":
+            app2.run()
+        elif app_choice == "App 3":
+            app3.run()
+
+if __name__ == "__main__":
+    main()
